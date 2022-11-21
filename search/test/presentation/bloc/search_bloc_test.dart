30f233e82615beecaff3777d42b3b81779a05a05
fazaimron27 <cincaumyself@gmail.com>
2022-11-21 19:33:08 +0700

    test: state management bloc testing for search functionality

---
+++ search/test/presentation/bloc/search_bloc_test.dart
@@ -0,0 +1,150 @@
+import 'package:core/utils/failure.dart';
+import 'package:core/domain/entities/movie/movie.dart';
+import 'package:core/domain/entities/tv/tv.dart';
+import 'package:search/domain/usecases/search_movies.dart';
+import 'package:search/domain/usecases/search_tv.dart';
+import 'package:search/presentation/bloc/search_bloc.dart';
+import 'package:dartz/dartz.dart';
+import 'package:mockito/mockito.dart';
+import 'package:bloc_test/bloc_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'search_bloc_test.mocks.dart';
+
+@GenerateMocks([SearchMovies, SearchTv])
+void main() {
+  late SearchMovieBloc searchMovieBloc;
+  late SearchTvBloc searchTvBloc;
+  late MockSearchMovies mockSearchMovies;
+  late MockSearchTv mockSearchTv;
+
+  setUp(() {
+    mockSearchMovies = MockSearchMovies();
+    mockSearchTv = MockSearchTv();
+    searchMovieBloc = SearchMovieBloc(mockSearchMovies);
+    searchTvBloc = SearchTvBloc(mockSearchTv);
+  });
+
+  group('search movies', () {
+    test('initial state should be empty', () {
+      expect(searchMovieBloc.state, SearchEmpty());
+    });
+
+    final tMovieModel = Movie(
+      adult: false,
+      backdropPath: '/muth4OYamXf41G2evdrLEg8d3om.jpg',
+      genreIds: const [14, 28],
+      id: 557,
+      originalTitle: 'Spider-Man',
+      overview:
+          'After being bitten by a genetically altered spider, nerdy high school student Peter Parker is endowed with amazing powers to become the Amazing superhero known as Spider-Man.',
+      popularity: 60.441,
+      posterPath: '/rweIrveL43TaxUN0akQEaAXL6x0.jpg',
+      releaseDate: '2002-05-01',
+      title: 'Spider-Man',
+      video: false,
+      voteAverage: 7.2,
+      voteCount: 13507,
+    );
+    final tMovieList = <Movie>[tMovieModel];
+    const tQuery = 'spiderman';
+
+    blocTest<SearchMovieBloc, SearchState>(
+      'Should emit [Loading, HasData] when data is gotten successfully',
+      build: () {
+        when(mockSearchMovies.execute(tQuery))
+            .thenAnswer((_) async => Right(tMovieList));
+        return searchMovieBloc;
+      },
+      act: (bloc) => bloc.add(const OnQueryChanged(tQuery)),
+      wait: const Duration(milliseconds: 500),
+      expect: () => [
+        SearchLoading(),
+        SearchMovieHasData(tMovieList),
+      ],
+      verify: (bloc) {
+        verify(mockSearchMovies.execute(tQuery));
+      },
+    );
+
+    blocTest<SearchMovieBloc, SearchState>(
+      'Should emit [Loading, Error] when get search is unsuccessful',
+      build: () {
+        when(mockSearchMovies.execute(tQuery)).thenAnswer(
+            (_) async => const Left(ServerFailure('Server Failure')));
+        return searchMovieBloc;
+      },
+      act: (bloc) => bloc.add(const OnQueryChanged(tQuery)),
+      wait: const Duration(milliseconds: 500),
+      expect: () => [
+        SearchLoading(),
+        const SearchError('Server Failure'),
+      ],
+      verify: (bloc) {
+        verify(mockSearchMovies.execute(tQuery));
+      },
+    );
+  });
+
+  group('search tv', () {
+    test('initial state should be empty', () {
+      expect(searchTvBloc.state, SearchEmpty());
+    });
+
+    final tTvModel = Tv(
+      backdropPath: '/etj8E2o0Bud0HkONVQPjyCkIvpv.jpg',
+      firstAirDate: '2022-08-21',
+      genreIds: const [10765, 18, 10759],
+      id: 94997,
+      name: 'House of the Dragon',
+      originCountry: const ['US'],
+      originalLanguage: 'en',
+      originalName: 'House of the Dragon',
+      overview:
+          'The Targaryen dynasty is at the absolute apex of its power, with more than 15 dragons under their yoke. Most empires crumble from such heights. In the case of the Targaryens, their slow fall begins when King Viserys breaks with a century of tradition by naming his daughter Rhaenyra heir to the Iron Throne. But when Viserys later fathers a son, the court is shocked when Rhaenyra retains her status as his heir, and seeds of division sow friction across the realm.',
+      popularity: 2620.574,
+      posterPath: '/1X4h40fcB4WWUmIBK0auT4zRBAV.jpg',
+      voteAverage: 8.5,
+      voteCount: 2339,
+    );
+    final tTvList = <Tv>[tTvModel];
+    const tQuery = 'house of the dragon';
+
+    blocTest<SearchTvBloc, SearchState>(
+      'Should emit [Loading, HasData] when data is gotten successfully',
+      build: () {
+        when(mockSearchTv.execute(tQuery))
+            .thenAnswer((_) async => Right(tTvList));
+        return searchTvBloc;
+      },
+      act: (bloc) => bloc.add(const OnQueryChanged(tQuery)),
+      wait: const Duration(milliseconds: 500),
+      expect: () => [
+        SearchLoading(),
+        SearchTvHasData(tTvList),
+      ],
+      verify: (bloc) {
+        verify(mockSearchTv.execute(tQuery));
+      },
+    );
+
+    blocTest<SearchTvBloc, SearchState>(
+      'Should emit [Loading, Error] when get search is unsuccessful',
+      build: () {
+        when(mockSearchTv.execute(tQuery)).thenAnswer(
+            (_) async => const Left(ServerFailure('Server Failure')));
+        return searchTvBloc;
+      },
+      act: (bloc) => bloc.add(const OnQueryChanged(tQuery)),
+      wait: const Duration(milliseconds: 500),
+      expect: () => [
+        SearchLoading(),
+        const SearchError('Server Failure'),
+      ],
+      verify: (bloc) {
+        verify(mockSearchTv.execute(tQuery));
+      },
+    );
+  });
+}
