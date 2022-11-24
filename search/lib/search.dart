2300964fc2d4f7f4d63760ee354f3a6f2b2ecfb9
fazaimron27 <cincaumyself@gmail.com>
2022-11-24 22:52:30 +0700

    feat: export all necessary in search module and delete provider export

--- search/lib/search.dart
+++ search/lib/search.dart
@@ -2,7 +2,6 @@ library search;
 
 export 'domain/usecases/search_movies.dart';
 export 'domain/usecases/search_tv.dart';
-export 'presentation/provider/movie_search_notifier.dart';
-export 'presentation/provider/tv_search_notifier.dart';
 export 'presentation/pages/movie_search_page.dart';
 export 'presentation/pages/tv_search_page.dart';
+export 'presentation/bloc/search_bloc.dart';
