5b1a98eca6dbb97f52f5f788fca8fbdad865d2d9
fazaimron27 <cincaumyself@gmail.com>
2022-11-25 19:15:16 +0700

    chore: fix typo in syntax writing

--- movie/lib/domain/usecases/get_watchlist_status.dart
+++ movie/lib/domain/usecases/get_watchlist_status.dart
@@ -1,9 +1,9 @@
 import '../repositories/movie_repository.dart';
 
-class GetWatchListStatus {
+class GetWatchlistStatus {
   final MovieRepository repository;
 
-  GetWatchListStatus(this.repository);
+  GetWatchlistStatus(this.repository);
 
   Future<bool> execute(int id) async {
     return repository.isAddedToWatchlist(id);
