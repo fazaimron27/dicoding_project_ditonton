afdbdf02b0b89105d8ee867c1afd0de5ebd8230c
fazaimron27 <cincaumyself@gmail.com>
2022-11-25 19:24:26 +0700

    refactor: importing bloc state management and removing import for provider state management

--- movie/lib/movie.dart
+++ movie/lib/movie.dart
@@ -10,12 +10,6 @@ export 'presentation/pages/now_playing_movies_page.dart';
 export 'presentation/pages/popular_movies_page.dart';
 export 'presentation/pages/top_rated_movies_page.dart';
 export 'presentation/pages/watchlist_movies_page.dart';
-export 'presentation/provider/movie_detail_notifier.dart';
-export 'presentation/provider/movie_list_notifier.dart';
-export 'presentation/provider/now_playing_movies_notifier.dart';
-export 'presentation/provider/popular_movies_notifier.dart';
-export 'presentation/provider/top_rated_movies_notifier.dart';
-export 'presentation/provider/watchlist_movie_notifier.dart';
 export 'domain/usecases/get_movie_detail.dart';
 export 'domain/usecases/get_movie_recommendations.dart';
 export 'domain/usecases/get_now_playing_movies.dart';
@@ -25,3 +19,5 @@ export 'domain/usecases/get_watchlist_movies.dart';
 export 'domain/usecases/get_watchlist_status.dart';
 export 'domain/usecases/remove_watchlist.dart';
 export 'domain/usecases/save_watchlist.dart';
+
+export 'presentation/bloc/movie_bloc.dart';
